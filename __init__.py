79a46ef92a9f50fb8f76afa6e624d12f01bba546
QiGao <147978754+CamelliaQg118@users.noreply.github.com>
2025-11-01 17:49:41 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
 from .utils import fix_seed, load_data
 from .STHGRL_model import sthgrl
-from .STHGRL_model_xiaohy import sthgrl_xiaohy
-from .STHGRL_model_degs import sthgrl_degs
 # from .GNNs import GAT, GCN
 from .clustering import mclust_R, leiden, louvain
 
 # from module import *
-# 此时被导入模块若定义了__all__属性，则只有__all__内指定的属性、方法、类可被导入。
-# 若没定义，则导入模块内的所有公有属性，方法和类 。
+
 __all__ = [
     "fix_seed",
     "sthgrl",
@@ -15,4 +12,4 @@ __all__ = [
     "sthgrl_xiaohy",
     "mclust_R"
 ]
-#相当于在from SDER_model import * 实际上只有Sedr类被导入，若Sedr不写入all中则导入的是SDER_model中全部的类或者函数
+
